cb0bfb584eb19743c6788c19e5c4ca06adbfdc3c
551 <wdf@hotmail.co.uk>
2026-09-10 14:09:25 +0100

    Wire Settings actions for v1.0.6

--- prefs/OBDUBRootListController.h
+++ prefs/OBDUBRootListController.h
@@ -1,6 +1,11 @@
 #import <Preferences/PSListController.h>
 
+@class PSSpecifier;
+
 @interface OBDUBRootListController : PSListController
+- (id)readPreferenceValue:(PSSpecifier *)specifier;
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier;
 - (void)resetWorkingDefaults;
+- (void)respring;
 - (void)openGitHub;
 @end
